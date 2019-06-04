3d3bb8c1f4bf04492ba8c90f833b771307a5b2fd
thenlvnp <42989819+thenlvnp@users.noreply.github.com>
2019-06-04 17:52:22 +0800

    Added individual components files
    Partially Finished agent dashboard tabs not including the availability section

--- app/js/app.js
+++ app/js/app.js
@@ -95,3 +95,89 @@ function closeItems() {
     item.className = item.className.replace("form--options-expanded", "");
   });
 }
+
+// CALENDAR
+
+function daysInMonth(month, year) {
+  return 32 - new Date(year, month, 32).getDate();
+}
+
+function showCalendar(month, year) {
+  let firstDay = new Date(year, month).getDay();
+
+  tbl = document.getElementById("calendar-body"); // body of the calendar
+  // clearing all previous cells
+  tbl.innerHTML = "";
+
+  // filing data about month and in the page via DOM.
+  monthAndYear.innerHTML = months[month] + " " + year;
+  selectYear.value = year;
+  selectMonth.value = month;
+
+  let date = 1;
+  for (let i = 0; i < 6; i++) {
+    let row = document.createElement("tr");
+
+    // created individual cells and fill with data
+
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < firstDay) {
+        cell = document.createElement("td");
+        cellText = document.createTextNode("");
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      } else if (date > daysInMonth(month, year)) {
+        break;
+      } else {
+        cell = document.createElement("td");
+        cellText = document.createTextNode(date);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+        date++;
+      }
+      tbl.appendChild(row); // appending each row into calendar body.
+    }
+  }
+}
+
+today = new Date();
+currentMonth = today.getMonth();
+currentYear = today.getFullYear();
+selectYear = document.getElementById("year");
+selectMonth = document.getElementById("month");
+
+months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+
+monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+function next() {
+  currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+  currentMonth = (currentMonth + 1) % 12;
+  showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+  currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+  currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+  showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+  currentYear = parseInt(selectYear.value);
+  currentMonth = parseInt(selectMonth.value);
+  showCalendar(currentMonth, currentYear);
+}
