59f33efc4f084a85242bf876f7a287ee584fbf83
thenlvnp <42989819+thenlvnp@users.noreply.github.com>
2019-07-01 17:09:28 +0800

    Moved composer.js

--- app/index.php
+++ app/index.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Home</title>
-    <link rel="stylesheet" href="./css/main.css" />
-    <script defer src="js/app.js"></script>
+    <link rel="stylesheet" href="/css/main.css" />
+    <script defer src="/js/app.js"></script>
     <script
       defer
       src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
